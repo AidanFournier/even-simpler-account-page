df703e02e1023a50d60d37ea7bf167c4ae1bd67e
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-02-25 21:57:36 +0900

    Updated users controller with sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.create(user_params)
-    if @user.after_save
-      redirect_to profile_path
-    else
-      render 'new'
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else 
+      render :new
     end
   end
 
@@ -16,23 +19,37 @@ class UsersController < ApplicationController
   end
 
   def edit
+    if !logged_in? || !@user
+      redirect_to root_path
+    end
   end
-
+      
   def update
-    @user = User.find(params[id])
-    if @user.update(user_params)
-      redirect_to profile_path
+    if @user
+      @user.update(user_params)
+
+      if @user.errors.any?
+        render "edit"
+      else
+        redirect_to user_path(@user)
+      end
     else
-      render 'new'
-    end 
+      render "edit"
+    end
   end
 
   def destroy
+    @user.destroy
+    redirect_to root_path
   end
 
   private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
-  end 
+  end
+
+  def set_user
+    @user = User.find_by_id(params[:id])
+  end
 end

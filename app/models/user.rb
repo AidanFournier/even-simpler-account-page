5215b37c59dfe172b2e01c8c3875feb91a8445ac
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-03-13 16:49:05 +0900

    Added length validations to first and last name, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
 
     has_secure_password
     
-    validates :first_name, presence: true
-    validates :last_name, presence: true
+    validates :first_name, presence: true, length: { minimum: 2 }
+    validates :last_name, presence: true, length: { minimum: 2 }
 
     validates :email, presence: true, uniqueness: true
     validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
     
-    validates :password, presence: true
+    validates :password, presence: true, length: { in: 6..25 }
 end
